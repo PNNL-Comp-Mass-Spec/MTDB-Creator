394f43c592f04666a0ea6a97705930cd747166c6
Matthew Monroe <matthew.monroe@pnnl.gov>
2010-12-07 19:46:22 +0000

    Updated Readme.txt and RevisionHistory.txt Added Batch files for assembling distribution .Zip files

---
+++ MTDBCreator_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
@@ -0,0 +1,9 @@
+@echo on
+Del NETPredictionUtility_Source*.zip
+
+@echo off
+echo;
+
+Copy "F:\My Documents\Projects\DataMining\NET_Prediction_Utility\NETPredictionUtility_SourceCode\*Source*.zip" .
+
+
