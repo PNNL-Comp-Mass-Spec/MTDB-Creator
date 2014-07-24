b50cce4027ee5a94ace13d8e082b169e928c9a5e
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2014-07-24 12:10:08 -0700

    More TestSuite Changes

---
+++ MTDBCreatorTestSuite/IO/MTDBFramework_MtdbCreatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace MTDBCreatorTestSuite.IO
+{
+	[TestFixture]
+	class MTDBFramework_MtdbCreatorTests : TestBase
+	{
+		[TestFixtureSetUp]
+		public void DeleteOutputFiles()
+		{
+			var paths = new List<string>();
+			paths.Add("Output2.mtdb");
+			paths.Add("Output3.mtdb");
+			paths.Add("Output4.mtdb");
+			foreach (var path in paths)
+			{
+				if (File.Exists(path))
+				{
+					File.Delete(path);
+				}
+			}
+		}
+
+		[Test]
+		[TestCase(@"Output2.mtdb", @"Mzml\61928_SCU_WS_UPool_24_17Sep13_Cheetah_13-07-22_msgfplus.mzid", "", Ignore = false)]
+		[TestCase(@"Output3.mtdb", @"Mzml\QC_Shew_13_05-1_03_8Jun14_Samwise_14-02-16_msgfplus.mzid", @"Mzml\QC_Shew_13_05-2_03_8Jun14_Samwise_14-02-17_msgfplus.mzid", Ignore = false)]
+		public void TestWriteMZID(string outputPath, params string[] paths)
+		{
+			var mtdbCreator = new MTDBFramework.MtdbCreator();
+			var testPaths = new List<string>();
+			foreach (var path in paths)
+			{
+				if (!string.IsNullOrWhiteSpace(path))
+				{
+					testPaths.Add(GetPath(path));
+				}
+			}
+			mtdbCreator.CreateDB(testPaths, GetTestSuiteDataPath(outputPath));
+		}
+
+		[Test]
+		[TestCase(@"Output4.mtdb", @"Mzml\61928_SCU_WS_UPool_24_17Sep13_Cheetah_13-07-22_msgfplus.mzid", Ignore = false)]
+		[TestCase(@"Output4.mtdb", @"Mzml\QC_Shew_13_05-1_03_8Jun14_Samwise_14-02-16_msgfplus.mzid", Ignore = false)]
+		[TestCase(@"Output4.mtdb", @"Mzml\QC_Shew_13_05-2_03_8Jun14_Samwise_14-02-17_msgfplus.mzid", Ignore = false)]
+		[TestCase(@"Output4.mtdb", @"Mzml\QC_Shew_13_05-2_03_8Jun14_Samwise_14-02-17_msgfplus.mzid", Ignore = false)]
+		public void TestAppendMZID(string path, string outputPath)
+		{
+			var mtdbCreator = new MTDBFramework.MtdbCreator();
+			var testPaths = new List<string>();
+			testPaths.Add(GetPath(path));
+			mtdbCreator.CreateDB(testPaths, GetTestSuiteDataPath(outputPath));
+		}
+    }
+}
