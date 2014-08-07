65ee668d9e5b2a9a14ebb2b1f660baa1f8b17ca8
GrantFujimoto <grant.fujimoto@pnnl.gov>
2014-08-07 16:02:26 -0700

    Added MTDB Console project to MTDB Creator

---
+++ MTDBConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using MTDBFramework;
+
+namespace MTDBConsole
+{
+	class Program
+	{
+		public static CommandOptions CommandOptions;
+
+		static Program()
+		{
+			CommandOptions = new CommandOptions();
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("************MTDB Creator************");
+			Console.WriteLine("-> Creating Database");
+			Parser.Default.ParseArguments(args, CommandOptions);
+
+			MtdbCreator.CreateDB(ReadMzidList(), CommandOptions.DatabasePath);
+			Console.WriteLine("MTDB Creator Complete!");
+		}
+
+		static List<string> ReadMzidList()
+		{
+			List<string> pathList = new List<string>();
+
+			using (StreamReader input = new StreamReader(CommandOptions.MzidListPath))
+			{
+				while (input.Peek() > -1)
+				{
+					string entry = input.ReadLine();
+					pathList.Add(entry);
+				}
+			}
+
+			return pathList;
+		}
+	}
+}
