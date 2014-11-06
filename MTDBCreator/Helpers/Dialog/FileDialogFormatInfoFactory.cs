59f906644f96e1914ef9507d8f37deaf40b2abd8
Michael Degan <plithnar@gmail.com>
2014-11-06 12:17:05 -0800

    removing unintended msgfplus filter on mzid file format.

--- MTDBCreator/Helpers/Dialog/FileDialogFormatInfoFactory.cs
+++ MTDBCreator/Helpers/Dialog/FileDialogFormatInfoFactory.cs
@@ -15,7 +15,7 @@ namespace MTDBCreator.Helpers.Dialog
                 case "MSGFPlus":
                     return new FileDialogFormatInfo("Open MSGF+ analysis files", "MSGF+ Analysis Files |*msgfdb_syn.txt", LcmsIdentificationTool.MsgfPlus);
                 case "MZIdentML":
-                    return new FileDialogFormatInfo("Open MZIdent analysis files", "MZIdentML Analysis Files |*msgfplus.mzid;*msgfplus.mzid.gz", LcmsIdentificationTool.MZIdentML);
+                    return new FileDialogFormatInfo("Open MZIdent analysis files", "MZIdentML Analysis Files |*.mzid;*.mzid.gz", LcmsIdentificationTool.MZIdentML);
                 case "MSAlign":
                     return new FileDialogFormatInfo("Open MSAlign analysis files", "MSAlign Analysis Files |*msalign_syn.txt", LcmsIdentificationTool.MSAlign);
                 case "Analysis Files":
