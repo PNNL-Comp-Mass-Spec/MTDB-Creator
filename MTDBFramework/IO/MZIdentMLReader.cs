e18b08d818d43b34839fca72f2b9ad860b94e10a
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2015-05-08 16:12:52 -0700

    Changed scan number logic for the MZIdentMLReader - the native ID is interpreted (if in an interpretable format) and used in place of the Scan Number CVParam; the Scan Number CVParam is an option, not a requirement in the mzIdentML specification.

--- MTDBFramework/IO/MZIdentMLReader.cs
+++ MTDBFramework/IO/MZIdentMLReader.cs
@@ -24,6 +24,140 @@ namespace MTDBFramework.IO
             ReaderOptions = options;
         }
 
+        #region NativeId Conversion
+        /// <summary>
+        /// Provides functionality to interpret a NativeID as a integer scan number
+        /// Code is ported from MSData.cpp in ProteoWizard
+        /// </summary>
+        public static class NativeIdConversion
+        {
+            private static Dictionary<string, string> ParseNativeId(string nativeId)
+            {
+                var tokens = nativeId.Split(new char[] { '\t', ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                var map = new Dictionary<string, string>();
+                foreach (var token in tokens)
+                {
+                    var equals = token.IndexOf('=');
+                    var name = token.Substring(0, equals);
+                    var value = token.Substring(equals + 1);
+                    map.Add(name, value);
+                }
+                return map;
+            }
+
+            /// <summary>
+            /// Performs a "long.TryParse" on the interpreted scan number (single shot function)
+            /// </summary>
+            /// <param name="nativeId"></param>
+            /// <param name="num"></param>
+            /// <returns></returns>
+            public static bool TryGetScanNumberLong(string nativeId, out long num)
+            {
+                return long.TryParse(GetScanNumber(nativeId), out num);
+            }
+
+            /// <summary>
+            /// Performs a "int.TryParse" on the interpreted scan number (single shot function)
+            /// </summary>
+            /// <param name="nativeId"></param>
+            /// <param name="num"></param>
+            /// <returns></returns>
+            public static bool TryGetScanNumberInt(string nativeId, out int num)
+            {
+                return int.TryParse(GetScanNumber(nativeId), out num);
+            }
+
+            /// <summary>
+            /// Returns the integer-only portion of the nativeID that can be used for a scan number
+            /// If the nativeID cannot be interpreted, the original value is returned.
+            /// </summary>
+            /// <param name="nativeId"></param>
+            /// <returns></returns>
+            public static string GetScanNumber(string nativeId)
+            {
+                // TODO: Add interpreter for Waters' S0F1, S1F1, S0F2,... format
+                //switch (nativeIdFormat)
+                //{
+                //    case MS_spectrum_identifier_nativeID_format: // mzData
+                //        return value(id, "spectrum");
+                //
+                //    case MS_multiple_peak_list_nativeID_format: // MGF
+                //        return value(id, "index");
+                //
+                //    case MS_Agilent_MassHunter_nativeID_format:
+                //        return value(id, "scanId");
+                //
+                //    case MS_Thermo_nativeID_format:
+                //        // conversion from Thermo nativeIDs assumes default controller information
+                //        if (id.find("controllerType=0 controllerNumber=1") != 0)
+                //            return "";
+                //
+                //        // fall through to get scan
+                //
+                //    case MS_Bruker_Agilent_YEP_nativeID_format:
+                //    case MS_Bruker_BAF_nativeID_format:
+                //    case MS_scan_number_only_nativeID_format:
+                //        return value(id, "scan");
+                //
+                //    default:
+                //        if (bal::starts_with(id, "scan=")) return value(id, "scan");
+                //        else if (bal::starts_with(id, "index=")) return value(id, "index");
+                //        return "";
+                //}
+                if (nativeId.Contains("="))
+                {
+                    var map = ParseNativeId(nativeId);
+                    if (map.ContainsKey("spectrum"))
+                    {
+                        return map["spectrum"];
+                    }
+                    if (map.ContainsKey("index"))
+                    {
+                        return map["index"];
+                    }
+                    if (map.ContainsKey("scanId"))
+                    {
+                        return map["scanId"];
+                    }
+                    if (map.ContainsKey("scan"))
+                    {
+                        return map["scan"];
+                    }
+                }
+
+                // No equals sign, don't have parser breakdown
+                // Or key data not found in breakdown of nativeId
+                return nativeId;
+            }
+
+            //public static string GetNativeId(string scanNumber)
+            //{
+            //    switch (nativeIdFormat)
+            //    {
+            //        case MS_Thermo_nativeID_format:
+            //            return "controllerType=0 controllerNumber=1 scan=" + scanNumber;
+            //
+            //        case MS_spectrum_identifier_nativeID_format:
+            //            return "spectrum=" + scanNumber;
+            //
+            //        case MS_multiple_peak_list_nativeID_format:
+            //            return "index=" + scanNumber;
+            //
+            //        case MS_Agilent_MassHunter_nativeID_format:
+            //            return "scanId=" + scanNumber;
+            //
+            //        case MS_Bruker_Agilent_YEP_nativeID_format:
+            //        case MS_Bruker_BAF_nativeID_format:
+            //        case MS_scan_number_only_nativeID_format:
+            //            return "scan=" + scanNumber;
+            //
+            //        default:
+            //            return "";
+            //    }
+            //}
+        }
+        #endregion
+
         private readonly Dictionary<string, DatabaseSequence> m_database = new Dictionary<string, DatabaseSequence>();
         private readonly Dictionary<string, PeptideRef> m_peptides = new Dictionary<string, PeptideRef>();
         private readonly Dictionary<string, PeptideEvidence> m_evidences = new Dictionary<string, PeptideEvidence>();
@@ -70,8 +204,23 @@ namespace MTDBFramework.IO
 
             public int IsoError { get; set; }
 
-            public int ScanNum { get; set; }
+            private int _scanNum = -1;
 
+            public int ScanNum
+            {
+                get
+                {
+                    int num;
+                    if (!string.IsNullOrWhiteSpace(NativeId) && NativeIdConversion.TryGetScanNumberInt(NativeId, out num))
+                    {
+                        return num;
+                    }
+                    return _scanNum;
+                }
+                set { _scanNum = value; }
+            }
+
+            public string NativeId { get; set; }
             #endregion
 
         }
@@ -508,6 +657,8 @@ namespace MTDBFramework.IO
             var specRes = new List<SpectrumIdItem>();
 
             reader.MoveToContent();
+            var nativeId = reader.GetAttribute("spectrumID");
+            int scanNum = -1;
             reader.ReadStartElement("SpectrumIdentificationResult"); // Throws exception if we are not at the "SpectrumIdentificationResult" tag.
             while (reader.ReadState == ReadState.Interactive)
             {
@@ -527,11 +678,9 @@ namespace MTDBFramework.IO
                         break;
                     case "cvParam":
                         // Schema requirements: zero to many instances of this element
-                        int value = Convert.ToInt32(reader.GetAttribute("value"));
-                        foreach (var item in specRes)
+                        if (reader.GetAttribute("accession") == "MS:1001115")
                         {
-                            item.ScanNum = value;
-                            m_specItems.Add(item.SpecItemId, item);
+                            scanNum = Convert.ToInt32(reader.GetAttribute("value"));
                         }
                         reader.Read(); // Consume the cvParam element (no child nodes)
                         break;
@@ -543,6 +692,12 @@ namespace MTDBFramework.IO
                         reader.Skip();
                         break;
                 }
+            }
+            foreach (var item in specRes)
+            {
+                item.ScanNum = scanNum;
+                item.NativeId = nativeId;
+                m_specItems.Add(item.SpecItemId, item);
             }
             reader.Close();
         }
