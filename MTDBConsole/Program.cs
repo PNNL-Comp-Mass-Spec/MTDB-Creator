43abbb7546feee50e9525ddd9fe2217d45b55247
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-18 16:36:23 -0700

    Use the command line parser in PRISM.dll

--- MTDBConsole/Program.cs
+++ MTDBConsole/Program.cs
@@ -1,35 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using CommandLine;
+using System.Reflection;
 using MTDBFramework;
+using PRISM;
 
 namespace MTDBConsole
 {
     class Program
     {
+        private const string PROGRAM_DATE = "October 18, 2017";
+
         public static CommandOptions CommandOptions;
+
+        public static string GetAppVersion()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version + " (" + PROGRAM_DATE + ")";
+
+            return version;
+        }
 
         static Program()
         {
             CommandOptions = new CommandOptions();
         }
 
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Console.WriteLine("************MTDB Creator************");
             Console.WriteLine("-> Creating Database");
-            Parser.Default.ParseArguments(args, CommandOptions);
 
-            MtdbCreator.CreateDB(ReadMzidList(), CommandOptions.DatabasePath);
+            var asmName = typeof(Program).GetTypeInfo().Assembly.GetName();
+            var exeName = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+            var version = GetAppVersion();
+
+            var parser = new CommandLineParser<CommandOptions>(asmName.Name, version)
+            {
+                ProgramInfo = "This program creates an AMT tag database.",
+
+                ContactInfo = "Program written by Michael Degan and Bryson Gibbons for the Department of Energy (PNNL, Richland, WA) in 2014" +
+                              Environment.NewLine + Environment.NewLine +
+                              "E-mail: proteomics@pnnl.gov" + Environment.NewLine +
+                              "Website: http://panomics.pnnl.gov/ or http://omics.pnl.gov or http://www.sysbio.org/resources/staff/",
+
+                 UsageExamples = {
+                     exeName + "/MzidListPath:MzidInfo.txt /DatabasePath:TestDatabase.db3",
+                     exeName + "/m:MzidInfo.txt /d:TestDatabase.db3"
+                }
+            };
+
+            var parseResults = parser.ParseArgs(args);
+            var options = parseResults.ParsedResults;
+
+            if (!parseResults.Success)
+            {
+                System.Threading.Thread.Sleep(1500);
+                return -1;
+            }
+
+            if (!options.ValidateArgs())
+            {
+                parser.PrintHelp();
+                System.Threading.Thread.Sleep(1500);
+                return -1;
+            }
+
+            options.OutputSetOptions();
+
+            var mzidFiles = ReadMzidList(options);
+
+            MtdbCreator.CreateDB(mzidFiles, options.DatabasePath);
+
             Console.WriteLine("MTDB Creator Complete!");
+
+            return 0;
         }
 
-        static List<string> ReadMzidList()
+        static List<string> ReadMzidList(CommandOptions options)
         {
             var pathList = new List<string>();
 
-            using (var input = new StreamReader(CommandOptions.MzidListPath))
+            using (var input = new StreamReader(options.MzidListPath))
             {
                 while (input.Peek() > -1)
                 {
