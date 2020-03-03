27e8cf42b22c97e94b4a7c8ad4e9bc24989db2d6
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2020-03-03 14:23:08 -0800

    Update another NuGet Package, and un-ignore another test.

--- MTDBCreatorTestSuite/Algorithms/Alignment/LcmsWarpTests.cs
+++ MTDBCreatorTestSuite/Algorithms/Alignment/LcmsWarpTests.cs
@@ -20,8 +20,7 @@ namespace MTDBCreatorTestSuite.Algorithms.Alignment
         /// </summary>
         /// <param name="paths"></param>
         [TestCase(@"QC_Shew_13_02_2a_03Mar14\QC_Shew_13_02_2a_03Mar14_Leopard_14-02-01_msgfplus_syn.txt",
-            @"QC_Shew_13_02_2b_03Mar14\QC_Shew_13_02_2b_03Mar14_Leopard_14-02-02_msgfplus_syn.txt", Ignore = "false"
-            )]
+            @"QC_Shew_13_02_2b_03Mar14\QC_Shew_13_02_2b_03Mar14_Leopard_14-02-02_msgfplus_syn.txt")]
         public void TestLcmsWarpWrite(params string[] paths)
         {
             var fullPaths        = new List<string>();
